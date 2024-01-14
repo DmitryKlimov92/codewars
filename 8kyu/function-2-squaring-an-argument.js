43c0759d7a6818fdec4100c59bef488f7a9f8b26
DmitryKlimov92 <the.cabbagecake@gmail.com>
2024-01-14 21:12:40 +0300

    gravity-flip.js

---
+++ 8kyu/function-2-squaring-an-argument.js
@@ -0,0 +1,15 @@
+/*
+        DESCRIPTION
+
+        Now you have to write a function that takes an argument and returns the square of it.
+
+ */
+
+
+
+
+
+
+// SOLUTION
+
+const square=(a)=> Math.pow(a,2);
